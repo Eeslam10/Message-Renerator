ceb07110d4c3174688e60583d875ceb99ec6d180
islam sani <islammuhd8@gmail.com>
2023-10-25 11:56:22 +0100

    completed a random message generator using javascript

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-// “Love all, trust a few, do wrong to none.” – ...
-// “You call it madness, but I call it love.” – ...
-// “We can only learn to love by loving.” – ...
-// “A life lived in love will never be dull.” – ...
-// “Life is the flower for which love is the honey.”
 
-console.log('hi')
+const messages = ["Love all, trust a few, do wrong to none", "You call it madness, but I call it love", "We can only learn to love by loving", "A life lived in love will never be dull", "Life is the flower for which love is the honey"]
+
+const generateMessage = () => {
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    randomMessages = messages[randomIndex];
+    console.log(randomMessages)
+};
+
+generateMessage()
+
